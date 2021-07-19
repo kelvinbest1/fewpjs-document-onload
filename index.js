8f21b132d49caff31f59ebc4c5a576305cdab06a
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-07-18 23:41:52 -0400

    commit tree fb7529370f5233f96aba72637002fccd47eda26c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {document.getElementById('text');
+document.textContent('This is really cool!');
+    
+  });
+  
+  
